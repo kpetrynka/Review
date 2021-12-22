d031970aaef42089268a1386a7c03f01bd429033
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-22 10:47:50 +0200

    improved with const

--- Tasks/3_step.js
+++ Tasks/3_step.js
@@ -1,11 +1,12 @@
 const exception = (array, ...except) => {
-  arrayKeys = Object.keys(array);
+  const arrayKeys = Object.keys(array);
     arrayKeys.forEach((i) => {
       if (except.includes(i)) { 
         delete array[i];
         }
     });
-  return array}
+  return array;
+}
        
 const result = exception({ a: 1, b: 'two', c: 3, d: 4 }, 'a', 'd');
 console.log(result);
